725e2acc9067bb11b155f689a85466b18da8fdec
Walid Saifi <saifiwalid21@gmail.com>
2024-08-29 14:03:07 +0100

    Update testting.java

--- src/gestion/testting.java
+++ src/gestion/testting.java
@@ -4,7 +4,7 @@ public class testting {
 	
 	
 	    public static void main(String[] args) {
-	        System.out.println("Hello, World!");
+	        System.out.println("Hello 1111  World!");
 	    }
 
 
