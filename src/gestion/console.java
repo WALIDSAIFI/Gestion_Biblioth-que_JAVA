b284b778a575fbe79c2d56d357466508a3522ea6
Walid Saifi <saifiwalid21@gmail.com>
2024-08-30 16:19:52 +0100

    Update console.java

--- src/gestion/console.java
+++ src/gestion/console.java
@@ -3,7 +3,13 @@ package gestion;
 public class console {
 	
 	public void  afficherMenu() {
-		
+		System.out.println("1-Ajouter un document\n");
+		System.out.println("2-Emprunter un document\n");
+		System.out.println("3-Retourner un document\n");
+		System.out.println("4-Afficher tous les documents\n");
+		System.out.println("5-Rechercher un document\n");
+		System.out.println("0-Quitter\n");
+		System.out.println("Entre votre Choix:");
 	}
 
 }
