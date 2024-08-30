287d6bb5652d2157ae25b26b9b7896ccc1eadb7d
Walid Saifi <saifiwalid21@gmail.com>
2024-08-30 11:59:16 +0100

    Update Livre.java

--- src/gestion/Livre.java
+++ src/gestion/Livre.java
@@ -1,5 +1,11 @@
 package gestion;
 
-public class Livre {
+public class Livre extends document{
+	private int isbn;
+
+	public Livre(String titre, int nombreDePages) {
+		super(titre, nombreDePages);
+		
+	}
 
 }
