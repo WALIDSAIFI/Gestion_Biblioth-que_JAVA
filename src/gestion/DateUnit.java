28071960df6ae50a84ddc3ffbffacc320e5b0dec
Walid Saifi <saifiwalid21@gmail.com>
2024-08-30 11:14:35 +0100

    Create DateUnit.java

---
+++ src/gestion/DateUnit.java
@@ -0,0 +1,12 @@
+package gestion;
+
+import java.text.SimpleDateFormat;
+
+public class DateUnit {
+
+	 public SimpleDateFormat Date;
+	
+	public DateUnit(SimpleDateFormat Date) {
+		this.Date = new SimpleDateFormat();
+	}
+}
