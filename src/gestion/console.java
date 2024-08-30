521484a8f7b614e53739911df25f1128958db663
Walid Saifi <saifiwalid21@gmail.com>
2024-08-30 16:20:54 +0100

    Update console.java

--- src/gestion/console.java
+++ src/gestion/console.java
@@ -8,8 +8,8 @@ public class console {
 		System.out.println("3-Retourner un document\n");
 		System.out.println("4-Afficher tous les documents\n");
 		System.out.println("5-Rechercher un document\n");
-		System.out.println("0-Quitter\n");
-		System.out.println("Entre votre Choix:");
+		System.out.println("6-Quitter\n");
+		System.out.println("Veuillez entrer votre choix (1-6) : ");
 	}
 
 }
