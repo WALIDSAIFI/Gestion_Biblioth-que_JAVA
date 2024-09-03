b150f48fac8a1ce76d00e5845f2164b8829fd47e
WALIDSAIFI <saifiwalid21@gmail.com>
2024-09-03 14:53:11 +0100

    ajouter methode emp ret......

--- src/gestion/Bibliotheque.java
+++ src/gestion/Bibliotheque.java
@@ -12,10 +12,36 @@ public class Bibliotheque {
         this.documentMap = new HashMap();
     }
 
-
  
+    public void ajouterDocument(document Doc){
+       this.documents.add(Doc);
+       this.documentMap.put(Doc.titre,Doc);
+    }
+    public void emprunterDocumment(int id){
+        document Document =documentMap.get(id);
+        if(Document !=null){
+            Document.emprunter();
+        }else{
+            System.out.println("Document non trouvé.");
+        }
 
+    }
 
+    public void retournerDocument(String id){
+        document Document = documentMap.get(id);
+        if(Document != null){
+            Document.retourner();
+        }else{
+            System.out.println("Document non trouvé.");
+        }
+    }
 
+    public void afficherDocuments() {
+        documents.forEach(document::afficherDetails);
+    }
+
+    public document rechercherDocument(String id) {
+        return documentMap.get(id);
+    }
 
 }
